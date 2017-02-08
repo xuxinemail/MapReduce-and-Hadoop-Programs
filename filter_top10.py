2022258e18782ebdf3879a2d073be713a23858bf
xuxinemail <xuxinemail@gmail.com>
2017-02-07 21:22:31 -0500

    Create filter_top10.py

---
+++ filter_top10.py
@@ -0,0 +1,53 @@
+#the question is asking for top 10 posts containing numbers ascending to descending
+#!/usr/bin/python
+"""
+Your mapper function should print out 10 lines containing longest posts, sorted in
+ascending order from shortest to longest.
+Please do not use global variables and do not change the "main" function.
+"""
+import sys
+import csv
+
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    lengths = []
+    ind = []
+    lines = []
+
+    for line in reader:
+        # YOUR CODE HERE
+        lines.append(line)
+        lengths.append(len(line[4]))
+    
+    ind = sorted(range(len(lengths)), key = lambda k:lengths[k], reverse = True)
+    for i, num in enumerate(ind):
+        if i < 10: 
+            writer.writerow(lines[ind[9-i]])
+
+
+
+test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
+"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
+
+main()
+
+# 
